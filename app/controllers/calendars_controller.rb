3a0e316cc671307700985b727ef47e156f6c744b
EC2 Default User <ec2-user@ip-172-31-43-25.us-east-2.compute.internal>
2020-01-25 09:48:24 +0000

    create calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,18 +3,83 @@ class CalendarsController < ApplicationController
   
   def index
     @dates = Time.zone.now
-    #@dates = {year: Time.zone.now.year, month: Time.zone.now.month}
-    #@timezone = Time.zone.now
     @messages = ""
   end
   
   def change
-    @dates = {year: params[:year], month: params[:month]}  #ここはindexと同じように修正が必要
+    @dates = Time.local(params[:year].to_i,params[:month].to_i)   #ここはindexと同じように修正が必要
+    @calendars = get_calendar(@dates)
     @messages = ""
   end
   
   private 
   def move_to_signin
     redirect_to new_user_session_path unless user_signed_in?
+  end
+  
+  #カレンダー情報を取得する
+  def get_calendar(m_dates)
+    calendar_arr = []
+    
+    #カレンダーが前の月を跨ぐ時
+    prev_month_arr = get_lastmanth_info(m_dates)
+    
+    #当月の情報を取得
+    curr_month_arr = get_currentmonth_info(m_dates)
+    
+    #来月の情報を取得
+    next_month_arr = get_nextmonth_info(m_dates)
+    
+    #表示するカレンダーの情報を１つn配列にまとめる
+    calendar_arr.push(prev_month_arr) if prev_month_arr != nil
+    calendar_arr.push(curr_month_arr)
+    calendar_arr.push(next_month_arr) if next_month_arr != nil
+    calendar_arr.flatten!
+    
+    return calendar_arr
+    
+  end
+  
+  ##カレンダーで表示する先月部分の情報取得
+  def get_lastmanth_info(m_dates)
+    firstday_of_month = m_dates.beginning_of_month
+    
+    #月の最初の曜日が日曜日ならnilを返す
+    return nil if firstday_of_month.wday == 0
+    
+    daysinfo_arr = []
+    for i in 0..firstday_of_month.wday-1
+      index = i - firstday_of_month.wday
+      hash_1dayinfo = {"day": firstday_of_month.ago(index.days).day, "month": "prev"}
+      daysinfo_arr.push(hash_1dayinfo) 
+    end
+    
+    return daysinfo_arr
+  end
+  
+  #カレンダーで表示する当月分の情報取得
+  def get_currentmonth_hash(m_dates)
+    
+    daysinfo_arr = []
+    for i in 1..m_dates.last_of_month.day
+      hash_1dayinfo = {"day": i, "month": "curr"}
+      daysinfo_arr.push(hash_1dayinfo)
+    end
+    
+    return daysinfo_arr
+  end
+  
+  #カレンダーで表示する来月分の情報取得
+  def get_nextmonth_info(m_dates)
+    last_of_month = m_dates.end_of_month
+    return nil if last_of_month.wday == 6 
+    
+    daysinfo_arr = []
+    for i in 1..(6 - last_of_month.wday)
+      hash_1dayinfo = {"day": i, "month": "next"}
+      daysinfo_arr.push(hash_1dayinfo)
+    end
+    
+    return daysinfo_arr
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "calendars#index"
-  post "change/year/:year/month/:month" => "calendars/change"
+  post "calendars/year/:year/month/:month" => "calendars#change"
 end
