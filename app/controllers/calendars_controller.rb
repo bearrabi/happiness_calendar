ee97d00d8d7af1608a499e8dba0ca8f7fb769564
EC2 Default User <ec2-user@ip-172-31-43-25.us-east-2.compute.internal>
2020-01-25 22:20:44 +0000

    add view calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,6 +3,7 @@ class CalendarsController < ApplicationController
   
   def index
     @dates = Time.zone.now
+    @calendars = get_calendar(@dates)
     @messages = ""
   end
   
@@ -21,22 +22,21 @@ class CalendarsController < ApplicationController
   def get_calendar(m_dates)
     calendar_arr = []
     
-    #カレンダーが前の月を跨ぐ時
-    prev_month_arr = get_lastmanth_info(m_dates)
-    
-    #当月の情報を取得
+    #先月、当月、来月の情報からカレンダーに表示する日付情報を取得
+    prev_month_arr = get_lastmanth_info(m_dates) if m_dates.beginning_of_month.wday != 0
     curr_month_arr = get_currentmonth_info(m_dates)
+    next_month_arr = get_nextmonth_info(m_dates) if m_dates.end_of_month.wday != 6
     
-    #来月の情報を取得
-    next_month_arr = get_nextmonth_info(m_dates)
-    
-    #表示するカレンダーの情報を１つn配列にまとめる
+    #表示するカレンダーの情報を１つの1一次元配列にまとめる
     calendar_arr.push(prev_month_arr) if prev_month_arr != nil
     calendar_arr.push(curr_month_arr)
     calendar_arr.push(next_month_arr) if next_month_arr != nil
     calendar_arr.flatten!
     
-    return calendar_arr
+    ##一次元配列から二次元配列に変換する
+    calendar_2darr = convert_1darr_to_2darr(7, calendar_arr)
+    
+    return calendar_2darr
     
   end
   
@@ -44,12 +44,9 @@ class CalendarsController < ApplicationController
   def get_lastmanth_info(m_dates)
     firstday_of_month = m_dates.beginning_of_month
     
-    #月の最初の曜日が日曜日ならnilを返す
-    return nil if firstday_of_month.wday == 0
-    
     daysinfo_arr = []
     for i in 0..firstday_of_month.wday-1
-      index = i - firstday_of_month.wday
+      index = (-1) * (i - firstday_of_month.wday)
       hash_1dayinfo = {"day": firstday_of_month.ago(index.days).day, "month": "prev"}
       daysinfo_arr.push(hash_1dayinfo) 
     end
@@ -58,10 +55,10 @@ class CalendarsController < ApplicationController
   end
   
   #カレンダーで表示する当月分の情報取得
-  def get_currentmonth_hash(m_dates)
+  def get_currentmonth_info(m_dates)
     
     daysinfo_arr = []
-    for i in 1..m_dates.last_of_month.day
+    for i in 1..m_dates.end_of_month.day
       hash_1dayinfo = {"day": i, "month": "curr"}
       daysinfo_arr.push(hash_1dayinfo)
     end
@@ -72,7 +69,6 @@ class CalendarsController < ApplicationController
   #カレンダーで表示する来月分の情報取得
   def get_nextmonth_info(m_dates)
     last_of_month = m_dates.end_of_month
-    return nil if last_of_month.wday == 6 
     
     daysinfo_arr = []
     for i in 1..(6 - last_of_month.wday)
@@ -82,4 +78,26 @@ class CalendarsController < ApplicationController
     
     return daysinfo_arr
   end
+  
+  ##一次元配列を二次元配列に変換する
+  def convert_1darr_to_2darr(m_div_count, m_source_arr)
+    
+    rtn_2darr = []
+    d1arr = []
+    
+    ##変換
+    m_source_arr.each_with_index do |src, i|
+      d1arr[i % m_div_count] = src
+      
+      ##区切る場所で一次元配列を二次元配列に格納
+      if (i % m_div_count == m_div_count - 1)
+        rtn_2darr.push(d1arr)
+        d1arr = []
+      end
+      
+    end
+    
+    return rtn_2darr
+  end
+  
 end
