fc1e7504c2f895d376ca9453e902083eb26739f0
EC2 Default User <ec2-user@ip-172-31-43-25.us-east-2.compute.internal>
2020-01-26 02:37:13 +0000

    add messages path to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :messages, expect[:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "calendars#index"
   post "calendars/year/:year/month/:month" => "calendars#change"
