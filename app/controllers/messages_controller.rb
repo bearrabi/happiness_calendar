2fd049bb22b2fdefe2aad12ad71ad7bc42481feb
EC2 Default User <ec2-user@ip-172-31-43-25.us-east-2.compute.internal>
2020-01-27 21:07:54 +0000

    add new btn

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,14 +7,24 @@ class MessagesController < ApplicationController
   def show
   end
   
+  def new
+  end
+  
+  def create
+  end
+  
   ##特定ユーザーかつ、特定の日付のメッセージリストを表示
   def show_parts
-    date = Time.local(params[:year].to_i,params[:month].to_i,params[:day].to_i)
-    @messages = Message.where(date: date)
+    date = Time.local(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    @messages = Message.where(date: date, user_id: current_user.id)
   end
   
   ##特定のユーザーのメッセージ全てを表示
   def show_all
-    
+    @messages = Message.where(user_id: current_user.id)
+  end
+  
+  def message_params
+    params.permit[:to_name, :to_email, :title, :contents, :user_id, :day_id]
   end
 end
